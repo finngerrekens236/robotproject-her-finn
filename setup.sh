9c7cbb34fffb7c7af6b9f40cf50f5f07f4a8e037
finngerrekens236 <flp.gerrekens@student.avans.nl>
2026-05-25 22:14:24 +0200

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Setup script voor ROS vision node dependencies
+# Gebruik: bash setup.sh
+
+echo "=== ROS Vision Package Setup ==="
+
+# Check Python version
+PYTHON_VERSION=$(python3 --version 2>&1 | awk '{print $2}')
+echo "Huidige Python versie: $PYTHON_VERSION"
+
+# Als Python < 3.8, upgrade naar 3.8
+MAJOR=$(echo $PYTHON_VERSION | cut -d. -f1)
+MINOR=$(echo $PYTHON_VERSION | cut -d. -f2)
+
+if [ "$MAJOR" -lt 3 ] || ([ "$MAJOR" -eq 3 ] && [ "$MINOR" -lt 8 ]); then
+    echo "Python 3.6 gedetecteerd, upgraden naar 3.8..."
+    sudo apt update
+    sudo apt install -y python3.8 python3.8-venv python3.8-dev
+    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+    echo "Python upgraded naar 3.8"
+fi
+
+# ROS dependencies (als ROS nog niet alles heeft)
+echo "ROS dependencies installeren..."
+sudo apt install -y ros-noetic-cv-bridge python3-rospy python3-yaml
+
+# pip3 packages
+echo "Python packages installeren..."
+pip3 install -r requirements.txt
+
+echo "=== Setup voltooid! ==="
+echo "Volgende stap: catkin build"
